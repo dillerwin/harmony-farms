a62ffe3e7c779ba9afc553d7cad15afc3ee6cb65
dillerwin <dillerwin@gmail.com>
2021-11-24 11:51:35 -0500

    fixed connection issue with database

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-import { Mongoose } from "mongoose";
 require("dotenv").config();
 
 const express = require("express");
@@ -10,10 +9,27 @@ const port = process.env.PORT || 8000;
 const app = express();
 const staticDir = process.env.DEV ? "./client/public" : "./client/build";
 
-const url = `mongodb+srv://${process.env.username}:${process.env.password}@cluster0.idqix.mongodb.net/`;
+const url = `mongodb+srv://${process.env.user}:${process.env.password}@cluster0.idqix.mongodb.net/harmony-farms`;
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.listen(port, () => {
+  console.log(`Hearing you on port: ${port}`);
+});
 
 app.use(express.static(staticDir));
 
 app.use(express.urlencoded({ extended: true }));
+
+const testSchema = new mongoose.Schema({
+  testContent: { type: String },
+});
+
+const Test = mongoose.model("Test", testSchema);
+
+app.post("/test", async (req, res) => {
+  let userObj = req.body;
+  let newTestObj = new Test(userObj);
+  await newTestObj.save();
+  res.send("posted");
+});
