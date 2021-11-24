9f559a32c702e3a7445d5edf7899e78788ba752c
dillerwin <dillerwin@gmail.com>
2021-11-24 10:35:41 -0500

    basic setup

---
+++ server.js
@@ -0,0 +1,18 @@
+import { Mongoose } from "mongoose";
+require("dotenv").config()
+
+const express = require("express")
+const path = require("path")
+const mongoose = require('mongoose')
+const db = mongoose.connection
+
+const port = process.env.PORT || 8000
+const app = express()
+
+const url = `mongodb+srv://${process.env.username}:${process.env.password}@cluster0.idqix.mongodb.net/`
+
+mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true})
+
+app.use(express.static(staticDir))
+
+app.use(express.urlencoded({extended: true}))
