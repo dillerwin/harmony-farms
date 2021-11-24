5bbb8445a34eac7e4486ad2e172cffe0c3714dae
dillerwin <dillerwin@gmail.com>
2021-11-24 11:01:48 -0500

    added staticDir

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 import { Mongoose } from "mongoose";
-require("dotenv").config()
+require("dotenv").config();
 
-const express = require("express")
-const path = require("path")
-const mongoose = require('mongoose')
-const db = mongoose.connection
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const db = mongoose.connection;
 
-const port = process.env.PORT || 8000
-const app = express()
+const port = process.env.PORT || 8000;
+const app = express();
+const staticDir = process.env.DEV ? "./client/public" : "./client/build";
 
-const url = `mongodb+srv://${process.env.username}:${process.env.password}@cluster0.idqix.mongodb.net/`
+const url = `mongodb+srv://${process.env.username}:${process.env.password}@cluster0.idqix.mongodb.net/`;
 
-mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 
-app.use(express.static(staticDir))
+app.use(express.static(staticDir));
 
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }));
