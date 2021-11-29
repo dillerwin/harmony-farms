1ab83e27870fc1d60283d18a4032e3eb89a38ec7
JSaturn9 <69169169+JSaturn9@users.noreply.github.com>
2021-11-29 16:41:53 -0500

    Created about us page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,28 @@
 import "./App.css";
+//Import BrowserRouter, Route, and Routes from react-router-dom to be used in component
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+//Imports the Home component
+//import Home from "./components/Home.js";
+//Imports the Shy Guy component
+import About from "./components/About.jsx";
 
+//Function for the main app
 function App() {
+  //Returns various routes within BrowserRouter
   return (
     <div className="App">
-      <form method="post" action="/test">
-        <input name="test" type="text"></input>
-      </form>
+      {/* BrowserRouter is used for client side routing with URL segments*/}
+      <BrowserRouter>
+        {/* Routes contains all of the routes */}
+        <Routes>
+          {/* Route is each individual route - utilizes the path and element prop. 
+          Path reflects the URL path, element is where component is rendered */}
+          {/* Routes to Home Component */}
+          {/* <Route path="/" element={<Home />} /> */}
+          {/* Routes to About Component */}
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
