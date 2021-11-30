f16717e90690451f706454a91c7cf0651a230cfa
benjaminragland <59846383+benjaminragland@users.noreply.github.com>
2021-11-30 16:37:03 -0500

    added Card/MeetButton/Quote Components

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import IntroImages from "./IntroImages";
 import HeroImage from "./Hero";
+import MeetButton from "./MeetButton";
+import QuoteBlockOne from "./QuoteBlockOne";
+import Cards from "./Cards";
 
 function Home() {
   return (
     <>
       <HeroImage />
       <IntroImages />
+      <MeetButton />
+      <QuoteBlockOne />
+      <Cards />
     </>
   );
 }
