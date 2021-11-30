657d93d554be0fdb2c3ca7c0ad600bd3bd3610be
JSaturn9 <69169169+JSaturn9@users.noreply.github.com>
2021-11-30 11:26:20 -0500

    Added Home component to Browser Router

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,8 @@ import "./App.css";
 //Import BrowserRouter, Route, and Routes from react-router-dom to be used in component
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 //Imports the Home component
-//import Home from "./components/Home.js";
-//Imports the Shy Guy component
+import Home from "./components/Home.jsx";
+//Imports the About component
 import About from "./components/About.jsx";
 
 //Function for the main app
@@ -20,7 +20,7 @@ function App() {
             {/* Route is each individual route - utilizes the path and element prop. 
           Path reflects the URL path, element is where component is rendered */}
             {/* Routes to Home Component */}
-            {/* <Route path="/" element={<Home />} /> */}
+            <Route path="/" element={<Home />} />
             {/* Routes to About Component */}
             <Route path="/about" element={<About />} />
           </Routes>
