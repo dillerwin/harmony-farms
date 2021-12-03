1d3345a8fd366e8654dca0363b341d09d8581b77
Ghislaine Lykens <glykens@Ghislaines-MacBook-Pro.local>
2021-12-03 15:51:22 -0500

    nav

--- client/src/components/Raffle.jsx
+++ client/src/components/Raffle.jsx
@@ -1,13 +1,32 @@
 //imports react into the component
 import React from "react";
 import RaffleImage from "./RaffleImage.jsx";
+import PrizeImages from "./PrizeImages.jsx";
+import QuoteBlockTwo from "./QuoteBlockTwo.jsx";
+import Footer from "./Footer.jsx";
+import Donate from "./Donate.jsx";
+import DonateHeader from "./DonateHeader.jsx";
+import RaffleDonate from "./RaffleDonate.jsx";
+import Winner from "./Winner.jsx";
 
 function Raffle() {
   return (
-    <div id="raffle">
+    <>
       <RaffleImage />
-      <h1>This will be the raffle page</h1>
-    </div>
+      <div>
+        <div className="raffle-header">
+          <Winner />
+        </div>
+        <h1 className="raffle-header">What's This?</h1>
+        <p className="money-text">
+          Every week, we here at Harmony Farms hold a raffle to give back to our
+          amazing donors!
+        </p>
+        <RaffleDonate />
+        <PrizeImages />
+        <Footer />
+      </div>
+    </>
   );
 }
 
