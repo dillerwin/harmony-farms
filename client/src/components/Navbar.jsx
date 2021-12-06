9fa6199c6624cda4a6e1025d1164d659833ef132
Andre Grant <andrethegrant@gmail.com>
2021-12-06 10:00:33 -0500

    pulling

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -10,16 +10,15 @@ import Home from "./Home";
 import WellnessCenter from "./WellnessCenter";
 import Raffle from "./Raffle";
 //import Events from './Events';
-import Services from './Services';
-import Volunteer from './Volunteer';
+import Services from "./Services";
+import Volunteer from "./Volunteer";
 //import './app.css';
 import Dropdown from "./Dropdown";
-import './Navbar.css';
+import "./Navbar.css";
 import { NavButton } from "./NavButton";
 import Logo from "./Logo";
 
 function Navbar() {
-  
   const [click, setClick] = useState(false);
   const [dropdown, setDropdown] = useState(false);
 
@@ -27,7 +26,7 @@ function Navbar() {
   const closeMobileMenu = () => setClick(false);
 
   const onMouseEnter = () => {
-    if(window.innerWidth < 960) {
+    if (window.innerWidth < 960) {
       setDropdown(false);
     } else {
       setDropdown(true);
@@ -41,59 +40,96 @@ function Navbar() {
       setDropdown(false);
     }
   };
-  
-// GB: Dill- moved your admin block to down below my hooks bc it was throwing error
+
+  // GB: Dill- moved your admin block to down below my hooks bc it was throwing error
   if (window.location.href.includes("/admin")) {
     return null;
-  };
+  }
 
-  return(
+  return (
     <>
-    <nav className="navbar">
-    
-      <div className="logo-image-wrapper">
-      <Link to="/" component={Home}>
-        <img src={Logo} className="logo-image" />
-      </Link>
-      </div>
+      <nav className="navbar">
+        <div className="logo-image-wrapper">
+          <Link to="/" component={Home}>
+            <img src={Logo} className="logo-image" />
+          </Link>
+        </div>
 
-      <div className="menu-icon" onClick={handleClick}>
-        <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
-      </div>
+        <div className="menu-icon" onClick={handleClick}>
+          <i className={click ? "fas fa-times" : "fas fa-bars"} />
+        </div>
 
-      <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-        {/* <li>
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
+          {/* <li>
           <Link to = "/" className="nav-item" component={Home} onClick={closeMobileMenu}>Home</Link>
         </li> */}
-        <li>
-          <Link to = "/about" className="nav-item" component={About} onClick={closeMobileMenu}>About</Link>
-        </li>
-        <li>
-          <Link to = "/animals" className="nav-item" component={Animals} onClick={closeMobileMenu}>Animals</Link>
-        </li>
-        <li>
-          <Link to = "/wellness-center" className="nav-item" component={WellnessCenter} onClick={closeMobileMenu}>Wellness Center</Link>
-        </li>
-        <li>
-          <Link to = "/raffle" className="nav-item" component={Raffle} onClick={closeMobileMenu}>&nbsp;Raffle&nbsp; </Link>
-        </li>
-        <li 
-        onMouseEnter={onMouseEnter}
-        onMouseLeave={onMouseLeave}>
-          <Link to = "/services" className="nav-item" component={Services} onClick={closeMobileMenu}>
-            Visit Us &nbsp;<i className="fas fa-caret-down"/>
+          <li>
+            <Link
+              to="/about"
+              className="nav-item"
+              component={About}
+              onClick={closeMobileMenu}
+            >
+              About
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/animals"
+              className="nav-item"
+              component={Animals}
+              onClick={closeMobileMenu}
+            >
+              Animals
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/wellness-center"
+              className="nav-item"
+              component={WellnessCenter}
+              onClick={closeMobileMenu}
+            >
+              Wellness Center
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/raffle"
+              className="nav-item"
+              component={Raffle}
+              onClick={closeMobileMenu}
+            >
+              &nbsp;Raffle&nbsp;{" "}
+            </Link>
+          </li>
+          <li onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
+            <Link
+              to="/services"
+              className="nav-item"
+              component={Services}
+              onClick={closeMobileMenu}
+            >
+              Visit Us &nbsp;
+              <i className="fas fa-caret-down" />
             </Link>
             {dropdown && <Dropdown />}
-        </li>
-        <li>
-          <Link to = "/contact" className="nav-item" component={Contact} onClick={closeMobileMenu}>Contact Us</Link>
-        </li>
-      </ul>
-      <div>
-      <NavButton />
-      </div>
-      
-    </nav>
+          </li>
+          <li>
+            <Link
+              to="/contact"
+              className="nav-item"
+              component={Contact}
+              onClick={closeMobileMenu}
+            >
+              Contact Us
+            </Link>
+          </li>
+        </ul>
+        <div>
+          <NavButton />
+        </div>
+      </nav>
     </>
   );
 }
