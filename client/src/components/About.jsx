0bc389fcb192285cbe6dc738881b3879d1a0830f
JSaturn9 <69169169+JSaturn9@users.noreply.github.com>
2021-12-09 10:00:57 -0500

    Working on about us page

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -46,6 +46,12 @@ function About() {
               are not just “products” but living, sentient beings capable of
               love, happiness, and understanding much like us.
             </p>
+            <h2 id="about-desc-heading">
+              Our Story
+            </h2>
+            <p id="about-desc">
+            For those of you who don’t know our story and our mission, here goes...For the past few years my husband and I have been dreaming about combining our passions, skills, and interests by founding an animal sanctuary and retreat center. Our mission is to promote healing and improve mental well-being through compassion and empathy. We offer animals that have suffered neglect or other abuse or who are at risk of being separated from their animal families a safe haven where they can live out the rest of their days in harmony. Jon is an army veteran and has a long history in the construction industry. Being a builder of all things has proven invaluable in starting a sanctuary from the ground up, especially with no existing farm buildings as our property was when we purchased it 4 months ago. Being a licensed therapist who specializes in working with children and families and is also trained in Animal-Assisted Play Therapy and Art Therapy, I plan to offer intensive family retreats on our 50 acre property to allow families to reconnect and heal in a natural and tranquil setting alongside the animals we rescue, many who have trauma stories of their own. Animals have long played a major role in the human experience, and many don’t even realize how much they actually help us, both emotionally and psychologically. We also want to educate the public and promote compassion and empathy by allowing them to experience farmed and often forgotten animals and see that they are not just “products” but living, sentient beings capable of love, happiness, and understanding much like us.
+            </p>
           </div>
         </div>
       </div>
