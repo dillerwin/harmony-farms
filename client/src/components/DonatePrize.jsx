b95dc809a35ca1fbd38dad3a0295e1c03b8f3f50
Andre Grant <andrethegrant@gmail.com>
2021-12-10 15:35:59 -0500

    finished raffle page

--- client/src/components/DonatePrize.jsx
+++ client/src/components/DonatePrize.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 import "../App.css";
-import SendMail from "./SendMail";
+import { Link } from "react-router-dom";
 
 function DonatePrize() {
   return (
     <>
       <div className="donate-request-container">
         <h2 className="donate-prize-header">
-          Want to donate a raffle prize? Send us an email!
+          We run ongoing weekly raffles and are always in need of prizes! If you
+          have goods or a service that would make a great raffle prize that
+          you'd like to donate, please email us the details!
         </h2>
         <div>
-          <SendMail
-            className="button money-button"
-            label="Email Us!"
-            mailto="mailto:harmonyfarmsanctuaryvt@gmail.com"
-          />
+          <Link to="/Contact">
+            <button className="button tour-button">Contact Us</button>
+          </Link>
         </div>
       </div>
     </>
